2fbb1c3417d61ea1d0426e4610c0626128e2552f
Charles Broskoski <broskoski@gmail.com>
2020-01-14 13:19:42 -0500

    Add metascraper-fulltext

--- index.js
+++ index.js
@@ -6,16 +6,40 @@ const metascraper = require("metascraper")([
   require("metascraper-logo")(),
   require("metascraper-publisher")(),
   require("metascraper-title")(),
-  require("metascraper-url")()
+  require("metascraper-url")(),
+  require("metascraper-fulltext")()
 ]);
 
 const got = require("got");
 
-const targetUrl =
-  "https://www.nytimes.com/2020/01/13/business/economy/iran-economy.html";
+module.exports.getURLMetadata = async event => {
+  const { queryStringParameters: params } = event;
 
-(async () => {
-  const { body: html, url } = await got(targetUrl);
+  if (!params || !params.url) {
+    return {
+      statusCode: 403,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({
+        error: "You must pass a url as a parameter (i.e. ?url=https://are.na)"
+      })
+    };
+  }
+
+  const { body: html, url } = await got(decodeURIComponent(params.url));
   const metadata = await metascraper({ html, url });
-  console.log(metadata);
-})();
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify({
+      url,
+      metadata
+    })
+  };
+
+  return response;
+};
